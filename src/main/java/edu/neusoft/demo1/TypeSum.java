c5aeec5912e6ff82350f62c6dd5dc450da9eb4b6
aitong22 <127023879+aitong22@users.noreply.github.com>
2025-04-17 18:50:00 +0800

    Add files via upload

---
+++ src/main/java/edu/neusoft/demo1/TypeSum.java
@@ -0,0 +1,21 @@
+package edu.neusoft.demo1;
+public class TypeSum{
+private String Type;
+private int sum;
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
